7d0ddad4889509413e772a1060198d265b6e14c4
justinrodrigues@live.nl <you@example.com>
2024-06-12 14:10:16 +0200

    Created Order Service

---
+++ OrderService/Program.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using CustomerService.Services;
+using CustomerService.Messaging;
+
+namespace OrderService
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Hello World!");
+        }
+
+        // This method is used to configure the host and services that the application will use, including consumers(endpoints)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMassTransitServices(ConfigureBusEndpoints);
+                    services.ConfigureHandlers();
+                });
+
+        // This method is used to configure the handlers that the application will use through DI
+        private static IServiceCollection ConfigureHandlers(this IServiceCollection services)
+        {
+            services.AddScoped<CustomerHandler>();
+            // This adds a service that will run in the background and send messages to the bus every 30 seconds for testing purposes
+            //services.AddHostedService<BusSenderBackgroundService>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddMassTransitServices(this IServiceCollection services, Action<IBusRegistrationConfigurator> busConfigure)
+        {
+            services.AddMassTransit(x =>
+            {
+                // Default settings
+                x.SetDefaultEndpointNameFormatter();
+
+                busConfigure(x);
+
+                // This configures rabbitmq, should be environment variables later on.
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.UseNewtonsoftJsonSerializer();
+                    cfg.UseNewtonsoftJsonDeserializer();
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            return services;
+        }
+
+        // This method is used to configure the endpoints that the application will use
+        private static void ConfigureBusEndpoints(IBusRegistrationConfigurator configurator)
+        {
+            // Add all consumers here for DI. This will allow the consumers to be resolved by the DI container
+            configurator.AddConsumer<RegisterCustomerServiceConsumer, RegisterCustomerServiceConsumerDefinition>();
+        }
+    }
+}
