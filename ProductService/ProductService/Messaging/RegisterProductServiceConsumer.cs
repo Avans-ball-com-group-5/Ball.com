9f07c103da6274a70216e93f7caf37fe283109dc
Jay_Moll <jaymoll95@gmail.com>
2024-06-22 11:24:26 +0200

    product setup done

---
+++ ProductService/ProductService/Messaging/RegisterProductServiceConsumer.cs
@@ -0,0 +1,22 @@
+﻿
+using MassTransit;
+using ProductDomain.Events.Input;
+using ProductService.Services;
+
+namespace ProductService.Messaging
+{
+    public class RegisterProductServiceConsumer : IConsumer<RegisterProductService>
+    {
+        private readonly ProductHandler _productHandler;
+        public RegisterProductServiceConsumer(ProductHandler productHandler)
+        {
+            _productHandler = productHandler;
+        }
+
+        public async Task Consume(ConsumeContext<RegisterProductService> context)
+        {
+            
+            await _productHandler.RegisterProductService(context.Message);
+        }
+    }
+}
---
+++ ProductService/ProductService/Services/ProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using ProductDomain.Events.Input;
+using ProductDomain.Events.Output;
+
+namespace ProductService.Services
+{
+    public class ProductHandler
+    {
+        private readonly IBus Bus;
+        public ProductHandler(IBus bus)
+        {
+            Bus = bus;
+        }
+
+        public async Task RegisterProductService(RegisterProductService registerEvent)
+        {
+            // Do something with the ticket info, like saving it to a database or sending it to another service
+            // For now, we'll just publish the created event
+            Console.WriteLine("Registering Product service...");
+
+            // Save the product to the database
+            // Send a message to the customer service to notify them of the new product
+            await Bus.Publish(
+                new ProductServiceRegistered(
+                    registerEvent.Id,
+                    registerEvent.Name,
+                    registerEvent.Description,
+                    registerEvent.Company,
+                    registerEvent.Price,
+                    registerEvent.Stock
+                    ));
+        }
+    }
+}
